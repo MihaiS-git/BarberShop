9c728c3676adeeaeb0e300938cf9cffd7a3e3fa6
MihaiS-git <ingsuciumihai@gmail.com>
2025-02-10 17:39:30 +0200

    fix router

--- barber-shop-ui/src/App.tsx
+++ barber-shop-ui/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+/* import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom"; */
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RootLayout from "./components/Root";
 import AuthenticationPage from "./pages/Authentication";
@@ -15,7 +16,7 @@ import AppointmentsPage from "./pages/Appointments";
 import AccountPage from "./pages/Account";
 import ErrorPage from "./pages/Error";
 
-const router = createBrowserRouter([
+/* const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout />,
@@ -78,7 +79,29 @@ const router = createBrowserRouter([
             }
         ],
     },
-]);
+]); */
+
+const router = createHashRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "login", element: <AuthenticationPage /> },
+        { path: "register", element: <RegistrationPage /> },
+        { path: "recover-password", element: <RecoverPasswordPage /> },
+        { path: "recover-password/:token", element: <NewPasswordPage /> },
+        { path: "treatments", element: <TreatmentsPage /> },
+        { path: "treatments/:barberId", element: <BarberTreatmentsPage /> },
+        { path: "barbers", element: <BarbersPage /> },
+        { path: "contact", element: <ContactPage /> },
+        { path: "cart", element: <CartPage /> },
+        { path: "appointments", element: <AppointmentsPage /> },
+        { path: "account", element: <AccountPage /> },
+        { path: "*", element: <ErrorPage /> }
+      ]
+    }
+  ]);
 
 function App() {
     return <RouterProvider router={router} />;
